27fdf317fd4d77defdbbc22b8f3b0369793232a6
Dominik <domco.salgovic@gmail.com>
2021-04-01 16:03:18 +0200

    Adapted script to migrate on startup

--- run.sh
+++ run.sh
@@ -3,4 +3,5 @@
 #script for use on server to launch the app with production environent, bound to port 3000 on ip adress
 #!!WORK IN PROGRESS
 
+rails db:migrate
 screen rails server --binding=139.162.130.177 -e production
